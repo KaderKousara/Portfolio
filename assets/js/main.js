90ac407f46311a8d1e40bab055df21de07550a63
AbdoKousara <abdelkader.kawsara@gmail.com>
2021-06-14 10:59:06 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+import Typed from '/assets/js/typed.js';
+
+var options = {
+strings: ['Web Developer', 'Nice Guy','Fast-learner'],
+typeSpeed: 50,
+backSpeed: 50,
+loop: true,
+};
+
+var typed = new Typed('.element', options);
+
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.querySelector(toggleId),
+    nav = document.querySelector(navId)
+
+    toggle.addEventListener('click', ()=>{
+        if(nav.classList.contains('show')){
+               nav.classList.remove('show');
+        }else{
+               nav.classList.add('show')
+        }
+ });
+}
+showMenu('.nav__toggle','.nav__list')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.button_resume',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
